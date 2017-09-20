d931ee295d63d59518c2e5222dd57c8e610e0a44
Lediet <n_lediet@tss-hr420-dm05.fc.ca>
2017-09-20 15:04:44 -0400

    listeners added to the dom elements.

--- script/main.js
+++ script/main.js
@@ -1,12 +1,17 @@
-(() => {
-   let images = document.querySelectorAll('.img-holder'),
-       theHeader = document.querySelectorAll('.header'),
-       theSubhead = document.querySelectorAll('.main-copy h2'),
-       theSeasonText = document.querySelector('.main-copy p'),
-       appliedClass;
+(() => { 
+    const theImages = document.querySelectorAll('.image-holder'),
+    theHeader = document.querySelectorAll('.header'),
+    theSubhead = document.querySelectorAll('.main-copy h2'),
+    theSeasonText = document.querySelector('.main-copy p');
+    let appliedClass;
 
     function changeElements() {
-        debugger;
+    console.log('good');
+    debugger;
     }
-    
-});
+
+    theImages.forEach((el, index) => {
+    // Loop through the photos and do some stuff
+    el.addEventListener('click', changeElements, false);
+    });
+})();
