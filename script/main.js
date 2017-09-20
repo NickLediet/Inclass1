a839e287dc17722833e4e7e77a5031dd5018fbc2
Lediet <n_lediet@tss-hr420-dm05.fc.ca>
2017-09-20 15:41:20 -0400

    the little app is now working

--- script/main.js
+++ script/main.js
@@ -1,17 +1,28 @@
 (() => { 
     const theImages = document.querySelectorAll('.image-holder'),
-    theHeader = document.querySelectorAll('.header'),
-    theSubhead = document.querySelectorAll('.main-copy h2'),
+    theHeader = document.querySelector('.heading'),
+    theSubhead = document.querySelector('.main-copy h2'),
     theSeasonText = document.querySelector('.main-copy p');
     let appliedClass;
 
     function changeElements() {
-    console.log('good');
-    debugger;
+        let subImages = document.querySelector('.subImagesContainer');
+        let objectIndex = dynamicContent[this.id];
+
+        theSubhead.classList.remove(appliedClass);
+        theHeader.classList.remove(appliedClass);
+        
+        theSubhead.classList.add(this.id);
+        theHeader.classList.add(this.id);
+
+        theSubhead.firstChild.nodeValue = objectIndex.headline;
+        theSeasonText.firstChild.nodeValue = objectIndex.text;
+
+        appliedClass = this.id;
     }
 
     theImages.forEach((el, index) => {
-    // Loop through the photos and do some stuff
-    el.addEventListener('click', changeElements, false);
+        // Loop through the photos and do some stuff
+        el.addEventListener('click', changeElements, false);
     });
 })();
