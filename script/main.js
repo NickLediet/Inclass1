b68c267ce8bd95e0cb3ab72fb0f70de9a7dcee75
Lediet <n_lediet@tss-hr420-dm05.fc.ca>
2017-09-20 14:45:23 -0400

    scafford up the elements

--- script/main.js
+++ script/main.js
@@ -1,1 +1,12 @@
+(() => {
+   let images = document.querySelectorAll('.img-holder'),
+       theHeader = document.querySelectorAll('.header'),
+       theSubhead = document.querySelectorAll('.main-copy h2'),
+       theSeasonText = document.querySelector('.main-copy p'),
+       appliedClass;
 
+    function changeElements() {
+        debugger;
+    }
+    
+});
